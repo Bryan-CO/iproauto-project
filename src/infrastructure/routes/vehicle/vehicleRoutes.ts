dd3b070ce2462bab017c6eee718a8a438b2e9942
bryan <luisbryann080403@gmail.com>
2024-10-08 23:32:47 -0500

    feat(infra): add brandRoutes in vehicleRoutes

--- src/infrastructure/routes/vehicle/vehicleRoutes.ts
+++ src/infrastructure/routes/vehicle/vehicleRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 import { VehicleController } from '../../controllers'
+import { BrandRouter } from '../brand/brandRoutes'
 
 // eslint-disable-next-line
 export class VehicleRouter {
   static getRoutes (): Router {
     const router = Router()
     router.get('/', VehicleController.getAllVehicles)
+
+    // Ruta anidada para las marcas
+    router.use('/brands', BrandRouter.getRoutes())
     return router
   }
 }
