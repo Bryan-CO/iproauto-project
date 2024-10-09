dac242ccdf733adc0bd0a60baaca4b9613fc0514
bryan <luisbryann080403@gmail.com>
2024-10-09 00:13:48 -0500

    feat: databaseConfig for .env added

---
+++ src/config/databaseConfig.ts
@@ -0,0 +1,7 @@
+export const DbConfig = {
+  HOST: process.env.DB_HOST ?? 'localhost',
+  PORT: Number(process.env.DB_PORT ?? 5432),
+  USERNAME: process.env.DB_USERNAME ?? 'postgres',
+  PASSWORD: process.env.DB_PASSWORD ?? 'postgres',
+  DATABASE: process.env.DB_DATABASE ?? 'iproauto'
+}
